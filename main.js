f6406e1613c852d511051e8d693892bec943b778
Leonardo Rogatti <leorrogatti@gmail.com>
2020-04-22 16:42:08 -0300

    Arrow functions

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-const arr = [1, 3, 4, 5, 8, 9];
+const arr = [1, 3, 4, 5, 6];
 
-const newArr = arr.map(function(item, index) {
-  return item + index;
-})
+const newArray = arr.map(item => item * 2);
+  
 
-console.log(newArr);
+console.log(newArray)
 
-const sum = arr.reduce(function(total, next) {
-   return total + next;
-});
+const teste = () => ({ nome: 'Leonardo' })
 
-console.log(sum);
-
-const filter = arr.filter(function(item) {
-  return item % 2 === 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function(item) {
-  return item === 4;
-});
-
-console.log(find)
+console.log(teste());
