5bf1fce8ad78f7d7836701771eb125af681ae4f5
Leonardo Rogatti <leorrogatti@gmail.com>
2020-04-23 16:59:30 -0300

    axios para pegar api do github/users/

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-  setTimeout(() => { resolve('Ok') }, 2000);
-});
+import axios from 'axios';
 
-// async function executaPromise() {
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
-// }
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);   
+      
+      console.log(response);
+    } catch (err) {
+      console.warn('Erro na API');
+    }
+  }
+}
 
-const executaPromise = async () => {
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-};
-
-executaPromise();
+Api.getUserInfo('leorogatti')
+Api.getUserInfo('leorogatti123123123')
